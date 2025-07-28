20c0fd818c5436953ff7f12293ec8d0694f71ec1
tattiRA120 <52059448+tattiRA120@users.noreply.github.com>
2025-07-28 19:33:12 +0900

    fix: Update news source logo URLs in config.

--- src/config.ts
+++ src/config.ts
@@ -28,8 +28,8 @@ export const OPENAI_EMBEDDING_MODEL = "text-embedding-3-small";
 export const CHUNK_SIZE = 1000; // OpenAI Batch API のチャンクサイズ
 
 export const NEWS_SOURCE_LOGOS: { [key: string]: string } = {
-    'Reuters': 'https://upload.wikimedia.org/wikipedia/commons/8/8d/Reuters_Logo.svg',
-    'Bloomberg': 'https://assets.bwbx.io/s3/navi/images/bloomberg_logo-2556aaa618.svg',
+    'Reuters': 'https://cdn.freebiesupply.com/logos/large/2x/reuters-6-logo-png-transparent.png',
+    'Bloomberg': 'https://images.ctfassets.net/lr0atmu04u9z/1PGS2NP94z5RP7L61GzIbm/85665ad1eadfd9c965448ad67097aba0/new_bloomberg_logosvg.png',
     'NHK': 'https://www3.nhk.or.jp/news/img/fb_futa16_600px.png',
     'ITmedia': 'https://is3-ssl.mzstatic.com/image/thumb/Podcasts115/v4/bd/0a/43/bd0a4322-3dce-7ae3-12fa-2625bd554cd1/mza_12590752468951394902.png/500x500bb.jpg',
     'ITmedia AI＋': 'https://is3-ssl.mzstatic.com/image/thumb/Podcasts115/v4/bd/0a/43/bd0a4322-3dce-7ae3-12fa-2625bd554cd1/mza_12590752468951394902.png/500x500bb.jpg',
@@ -37,13 +37,13 @@ export const NEWS_SOURCE_LOGOS: { [key: string]: string } = {
     'PC Watch': 'https://pc.watch.impress.co.jp/img/common/p01/logo/pcw.1200.png',
     'デジカメ Watch': 'https://dc.watch.impress.co.jp/include/common/p01/images/logo/dcw.1200.png',
     'AV Watch': 'https://av.watch.impress.co.jp/include/common/p01/images/logo/avw.1200.png',
-    'GIGAZINE': 'https://upload.wikimedia.org/wikipedia/commons/5/51/GIGAZINE_logo.svg',
-    'GIZMODO JAPAN': 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Gizmodo.svg',
+    'GIGAZINE': 'https://pbs.twimg.com/profile_images/876998955627827204/IuoxMaM2_400x400.jpg',
+    'GIZMODO JAPAN': 'https://upload.wikimedia.org/wikipedia/commons/6/69/Gizmodo_Media_Group_Logo.png',
     'エルミタージュ秋葉原': 'https://www.gdm.or.jp/wp-content/themes/hermitage/images/common/logo.png',
     'Zenn': 'https://static.zenn.studio/images/logo-only-dark.png',
-    'Qiita': 'https://upload.wikimedia.org/wikipedia/commons/4/49/Qiita_Logo.svg',
+    'Qiita': 'https://i.gyazo.com/43ef3dfee7b402b1582db2ce241731f5.png',
     'ギャズログ': 'https://gazlog.jp/wp-content/uploads/2024/02/cropped-Gazlog-favcon-3-1.jpg',
-    '北森瓦版': 'https://upload.wikimedia.org/wikipedia/commons/4/43/Feed-icon.svg',
+    '北森瓦版': 'https://cdn2.iconfinder.com/data/icons/social-icon-3/512/social_style_3_rss-512.png',
     // 汎用的なデフォルト画像
-    'DEFAULT': 'https://upload.wikimedia.org/wikipedia/commons/4/43/Feed-icon.svg',
+    'DEFAULT': 'https://cdn2.iconfinder.com/data/icons/social-icon-3/512/social_style_3_rss-512.png',
 };
